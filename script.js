27edfcc76c1ff6666166038de8fa3ed818943636
sathwik-89 <147127984+sathwik-89@users.noreply.github.com>
2025-10-26 18:24:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example interactivity
+const form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message!');
+});
